5f08ea6ddd389008fdb0693609b4c2d9a5c7e99f
Adam McSweeney <adammcsweeney@gmail.com>
2023-02-12 13:26:39 +0000

    cleaned up code and add comments.

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-Bugs to fixed:
-  - Pressing equals again will do the same calculation to the new answer, ie: answer + one - done
-  - Pressing am operand after equals will start a new sum - done
-  - Pressing a number after equals creates a new sum - done
-  - Pressing an operand twice - done
-  - pressing an operand before equals will activate calculation - done
-  - pressing an operand first - done
-  - pressing equals after a number returns number
+Calculator App For The Odin Project.
 
-Visual fixes
-  - Add spaces to display - done
+Bugs to be fixed:
+
   - Add comers to display - 
-  - Stop top display moving when entering into bottom display - 
+  - Stop top display moving when entering into bottom display
+  - If the answer is more than two lines then it should shrink rather than display out of the box. 
 
 Future features
   - Keyboard input
--- script.js
+++ script.js
@@ -1,116 +1,146 @@
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const prevNumberTextElement = document.querySelector('[data-prev-number]') 
-const currentNumberTextElement = document.querySelector('[data-current-number]') 
+//HTML Query Selectors.
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const prevNumberTextElement = document.querySelector('[data-prev-number]');
+const currentNumberTextElement = document.querySelector('[data-current-number]'); 
 
-var one = ''
-var two = ''
-var sum = ''
-var answer = ''
-var done = false
+//Global Varibles.
+let one = ''; //Stores the first operand.
+let two = ''; //Stores the second operand.
+let sum = ''; //Stores the operation symbol.
+let answer = ''; //Stores the result of the calculation.
+let done = false; //This used tis variable to check if a calculation has been done.
 
+
+//Updates Display with current numbers.
 function updateDisplay(topDisplay, bottomDisplay){
-  
-  document.getElementById("top").innerHTML = topDisplay
-  document.getElementById("bottom").innerHTML = bottomDisplay
-}
+  document.getElementById("top").textContent = topDisplay;
+  document.getElementById("bottom").textContent = bottomDisplay;
+};
 
+//Calculates the result of the expression stored in two and one using the operation stored in sum.
 function calculate(){
-  one = parseFloat(one)
-  two = parseFloat(two)
+  one = parseFloat(one);
+  two = parseFloat(two);
   switch(sum) {
       case '+':
-        answer = two + one
-        break
+        answer = two + one;
+        break;
       case '-':
-        answer = two - one
-        break
+        answer = two - one;
+        break;
       case '*':
-        answer = two * one
-        break
+        answer = two * one;
+        break;
       case '÷':
-        answer = two / one
-        break
-    }
-    var calculation = two + ' ' + sum + ' ' + one + ' ='
-    updateDisplay(calculation, answer)
-    equalsAgain = one
-    one = answer
-    done = true
-    sumActive = false
-}    
+        answer = two / one;
+        break;
+    };
+    //Stores the calculation expression as a string in the calculation variable.
+    var calculation = two + ' ' + sum + ' ' + one + ' =';
+    updateDisplay(calculation, answer);  //Calls the updateDisplay function and passes in calculation and answer.
+    equalsAgain = one;
+    one = answer;
+    done = true;
+    sumActive = false;
+};
 
+//Sets all global variables to their default values.
 function allClear() {
-  one = ''
-  two = ''
-  sum = ''
-  answer = ''
-  done = false
-  updateDisplay('', '')
-}
+  one = '';
+  two = '';
+  sum = '';
+  answer = '';
+  done = false;
+  updateDisplay('', '');
+};
 
+//Calls the calculate function if the sum variable is not an empty string
 function chooseOperation() {
-  if (sum === '') return
+  if (sum === '') return;
   if (sum !== '') {
-  calculate()
-  }
-}
-    
+  calculate();
+  };
+};
+
+//Adds an event listener to each number button element and sets the value of one when a number is clicked.
 numberButtons.forEach(button => [
   button.addEventListener('click', () => {
-  if (done == true) {
-      allClear()
-      done = false
-    }  
-    let entry = button.innerText
-    one = one + entry
-    updateDisplay(two + ' ' + sum, one) 
+  if (done == true) {     //If done is true, allClear function is called to reset all variables
+      allClear();
+      done = false;
+    }; 
+    let entry = button.innerText;
+    one = one + entry;
+    updateDisplay(two + ' ' + sum, one); 
   })
-])
+]);
 
+
+//This loop adds an event listener to each operation button element and sets the value of sum when
 operationButtons.forEach(button => [
+  //Adds click event listener to each operation button.
   button.addEventListener('click', () => {
+    // If one is empty, set it to 0
     if (one === '') {
-      one = '0'  
-    }
-    chooseOperation()
-    sum = button.innerText
+      one = '0' ; 
+    };
+    // If a previous operation exists, perform calculation.
+    chooseOperation();
+    // Set the operation to the current button's inner text.
+    sum = button.innerText;
+    //If a previous calculation has been done, use the result as two.
+    //otherwise, use one as two.
     if (done == true) {
-      two = answer
-      done = false
+      two = answer;
+      done = false;
     } else {
-        two = one
+        two = one;
       }
-    updateDisplay(one + ' ' + sum, '')
-    one = ''
+    //Update display with the operation and clear one
+    updateDisplay(one + ' ' + sum, '');
+    one = '';
   })
-])
+]);
 
+// event listener for the "equals" button
 equalsButton.addEventListener('click', button => { 
-    if (done == true) {
-      two = answer
-      one = equalsAgain
-      done = false
-      calculate()
-    } else if (two === '') {
-        return
-    } else if (one === '') {
-        one = two
-        calculate()
-    } else {
-        calculate()
-    }
-    //calculate()
-  }) 
+  // check if the calculation is already done
+  if (done == true) {
+    // if so, set the second value to the answer and set the first value to the previous second value
+    two = answer;
+    one = equalsAgain;
+    // reset the flag for done calculation
+    done = false;
+    // perform the calculation
+    calculate();
+  } else if (two === '') {
+      // if second value is not entered, do nothing
+      return;
+  } else if (one === '') {
+      // if first value is not entered, set the first value to the second value
+      one = two;
+      // perform the calculation
+      calculate();
+  } else {
+      // if both values are entered, perform the calculation
+      calculate();
+  };
+}); 
 
+// event listener for the "all clear" button
 allClearButton.addEventListener('click', button => {
-  allClear()
-})
+// call the function to clear all values and reset the display
+allClear();
+});
 
+// event listener for the "delete" button
 deleteButton.addEventListener('click', button => {
-  one = one.slice(0, -1)
-  updateDisplay('',one)
-})
+// remove the last character from the first value string
+one = one.slice(0, -1);
+// update the display with the new first value
+updateDisplay('',one);
+});
